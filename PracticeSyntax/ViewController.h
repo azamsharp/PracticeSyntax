46e30532cf5eb968bbc264a2da5161e0df82a081
azamsharp <azamsharp@gmail.com>
2016-07-01 12:13:41 -0500

    Practice Syntax

---
+++ PracticeSyntax/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PracticeSyntax
+//
+//  Created by Mohammad Azam on 7/1/16.
+//  Copyright © 2016 Mohammad Azam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PracticeSyntax/ViewController.m
@@ -0,0 +1,61 @@
+//
+//  ViewController.m
+//  PracticeSyntax
+//
+//  Created by Mohammad Azam on 7/1/16.
+//  Copyright © 2016 Mohammad Azam. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    
+    [super viewDidLoad];
+    
+    
+    // declare an int variable called x and assign the initial value 44
+    int x = 44;
+    
+    // declare an int variable called count and assign the initial value 10
+    
+    // declare a float variable called balance and assign initial value 245.34
+    
+    // declare a string variable called firstName and assign initial value "John "
+    
+    // declare a string variable called lastName and assign initial value "Doe"
+    
+    // declare a method called greet with following signature
+    // -(void) greet
+    
+    // declare a method called displayCount with following signature
+    //-(void) displayCount:(int) count
+    
+    // declare a method called displayFirstName with following signature
+      // -(void) displayFirstName:(NSString *) firstName
+    
+    // declare a method called displayLastName with following signature
+    // -(void) displayLastName:(NSString *) lastName
+    
+    // declare a string variable fullName. Concatenate firstName and lastName and assign the result to fullName
+    
+    // declare a method called displayFullName with the following signature
+    // -(void) displayFullName:(NSString *) fullName
+    
+    // declare a method called getMessage with the following signature
+    // -(NSString *) getMessage
+    
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
